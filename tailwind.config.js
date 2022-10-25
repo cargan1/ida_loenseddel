9ab621985617b0e69fc32c27af5280578ed8fe2d
Daniel Cargan <daniel@3wm.dkl>
2022-10-25 15:22:41 +0200

    Second round fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./index.html"],
-	theme: {
-		extend: {
-			colors: {
-				gray: "rgb(242, 242, 242)",
-				blue: "rgb(15, 69, 204)",
-				info: "rgb(15, 69, 204, 0.05)",
-				active: "rgb(15, 69, 204, 0.25)",
-				hover: "rgb(15, 69, 204, 0.15)",
-			},
-			fontFamily: {
-				montserrat: "'Montserrat', sans-serif",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        gray: "rgb(242, 242, 242)",
+        blue: "rgb(15, 69, 204)",
+        info: "rgb(15, 69, 204, 0.05)",
+        active: "rgb(15, 69, 204, 0.25)",
+        hover: "rgb(15, 69, 204, 0.15)",
+        grey: "#f9f9f9",
+      },
+      fontFamily: {
+        montserrat: "'Montserrat', sans-serif",
+      },
+    },
+  },
+  plugins: [],
 };
